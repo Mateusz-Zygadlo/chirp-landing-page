0194846ea628d1a830ce072a0679616b20dc7e0d
Mateusz <mateuszzygadlo00@gmail.com>
2022-01-04 14:00:06 +0100

    adding assets and design files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Home } from './pages'
 
 export const App = () => {
   return(
-    <div>
-      <h1 className="text-5xl">Landing page</h1>
-    </div>
+    <>
+      <Home />
+    </>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Navbar } from '../components'
+
+export const Home = () => {
+  return(
+    <div>
+      <Navbar />
+      <h1 className="text-5xl">Home page</h1>
+    </div>
+  )
+}
